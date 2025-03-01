d1552284d53f000e21b949f8d6a0699430d25862
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2025-03-01 00:28:39 -0500

    Nuke fyne

--- ui/init.go
+++ ui/init.go
@@ -1,18 +1,4 @@
 package ui
 
-import (
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-)
-
-var appInstance *fyne.App
-
 func Init() {
-	app := app.New()
-	appInstance = &app
-	CreateMainWindow(appInstance)
-}
-
-func GetApp() *fyne.App {
-	return appInstance
 }
